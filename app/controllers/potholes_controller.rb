577e21d4b95cdef2d38e175add0271f8080292d0
nokuthula kubheka <>
2025-01-21 19:31:16 +0200

    created new

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -2,5 +2,25 @@ class PotholesController < ApplicationController
   def index
     @potholes = Pothole.all
   end
-end
 
+  def new
+    @pothole = Pothole.new
+  end
+
+  def create
+    @pothole = Pothole.new(pothole_params)
+    @pothole.user = current_user
+    if @pothole.save
+      redirect_to @pothole, notice: 'Pothole was successfully reported.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pothole_params
+    params.require(:pothole).permit(:address, :size, :description, :status, :user_id)
+  end
+
+end
