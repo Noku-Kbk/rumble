e24bfe1752a803f7f5cb610ed9448a0d67975768
jennifer seqobane <jennifer.seqobane@gmail.com>
2025-01-23 21:06:15 +0200

    cloudinery

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Pothole.destroy_all
+User.destroy_all
 puts 'Seeding users and potholes...'
 
 # Create users
@@ -19,38 +21,40 @@ user2 = User.create!(
   password: 'password123'
 )
 
-# Create potholes
-Pothole.create!([
-  {
-    address: '123 Main St',
-    size: 'Large',
-    description: 'A massive pothole causing traffic issues.',
-    status: 'reported',
-    user: user1
-    .background-image: url('https://res.cloudinary.com/dva1lf16l/image/upload/v1737483236/potholes-1_mxeo94.webp');
-  },
-  {
-    address: '456 Elm St',
-    size: 'Medium',
-    description: 'A medium-sized pothole near the curb.',
-    status: 'in progress',
-    user: user2
-    .background-image: url('')
-  },
-  {
-    address: '789 Oak St',
-    size: 'Small',
-    description: 'A small but dangerous pothole in the middle of the road.',
-    status: 'fixed',
-    user: user1
-    .background-image: url('')
-  },
-  {
-    address: '101 Pine St',
-    size: 'Large',
-    description: 'Deep pothole on a busy highway, hazardous for cars.',
-    status: 'reported',
-    user: user2
-    .background-image: url('')
-  }
-])
+require "open-uri"
+
+file = URI.parse("https://res.cloudinary.com/dva1lf16l/image/upload/v1737483236/potholes-1_mxeo94.webp").open
+pothole = Pothole.new(address: '123 Main St',
+size: 'Large',
+description: 'A massive pothole causing traffic issues.',
+status: 'reported',
+user: user1)
+pothole.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+pothole.save
+
+file = URI.parse("https://res.cloudinary.com/dva1lf16l/image/upload/v1737488909/OIP_yiaozb.jpg").open
+pothole = Pothole.new(address: '456 Elm St',
+size: 'Medium',
+description: 'A medium-sized pothole near the curb.',
+status: 'In progress',
+user: user2)
+pothole.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+pothole.save
+
+file = URI.parse("https://res.cloudinary.com/dva1lf16l/image/upload/v1737483040/R_mqetbj.jpg").open
+pothole = Pothole.new(address: '789 Oak St',
+size: 'Small',
+description: 'A small but dangerous pothole in the middle of the road.',
+status: 'fixed',
+user: user2)
+pothole.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+pothole.save
+
+file = URI.parse("https://res.cloudinary.com/dva1lf16l/image/upload/v1737483119/OIP_miyhm6.jpg").open
+pothole = Pothole.new(address: '101 Pine St',
+size: 'Large',
+description: 'Deep pothole on a busy highway, hazardous for cars.',
+status: 'reported',
+user: user1)
+pothole.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+pothole.save
